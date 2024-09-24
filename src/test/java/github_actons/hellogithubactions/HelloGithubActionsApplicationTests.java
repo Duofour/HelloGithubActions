9f23f1fcbf9a24bfa3355ad0607ccae852fa86dc
Dufour <161844234+Duofour@users.noreply.github.com>
2024-09-24 10:34:54 +0200

    Initial commit

---
+++ src/test/java/github_actons/hellogithubactions/HelloGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package github_actons.hellogithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
